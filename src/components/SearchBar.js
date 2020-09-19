555d09e22a86b2de875cea608c5f73f60da4da16
Allen <headhuntar@gmail.com>
2020-09-19 11:23:04 -0700

    add state to searchbar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-class SearchBar extends React.Component {
-  render() {
-    return
-      <div>App</div>;
-  }
+const SearchBar = (props) => {
+
+  const [term, setTerm] = useState();
+
+  return (
+  <div className="search-bar ui segment">
+    <form onSubmit={e=> e.preventDefault} className="ui form">
+      <div className="field">
+        <label>Video Search</label>
+        <input
+          type="text"
+          value={term}
+          onChange={e=> setTerm(e.target.value)}
+        />
+      </div>
+    </form>
+  </div>
+  );
 }
 
 export default SearchBar;
