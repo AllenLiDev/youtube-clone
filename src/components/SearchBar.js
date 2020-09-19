24d5fadf0092f3a169fd7b287d1ee040e958185b
Allen <headhuntar@gmail.com>
2020-09-19 13:23:32 -0700

    update searchBar component to pass entered term

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,19 +4,24 @@ const SearchBar = (props) => {
 
   const [term, setTerm] = useState();
 
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    props.onFormSubmit(term);
+  }
+
   return (
-  <div className="search-bar ui segment">
-    <form onSubmit={e=> e.preventDefault} className="ui form">
-      <div className="field">
-        <label>Video Search</label>
-        <input
-          type="text"
-          value={term}
-          onChange={e=> setTerm(e.target.value)}
-        />
-      </div>
-    </form>
-  </div>
+    <div className="search-bar ui segment">
+      <form onSubmit={e => onFormSubmit(e)} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
+        </div>
+      </form>
+    </div>
   );
 }
 
