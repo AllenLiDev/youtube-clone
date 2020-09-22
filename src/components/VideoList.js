86e21c287f2d0701dfa0612b62142588dd661556
Allen <headhuntar@gmail.com>
2020-09-21 17:41:10 -0700

    use VideoItem in VideoList to generate list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = () => {
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem />
+  });
+
   return (
-    <div>Video List</div>
+    <div>{renderedList}</div>
   )
 };
 
